eac52b2e49ecb3354d3fa9a38ed7e491da3f2bcf
JaisonST <jaisonmanu@gmail.com>
2021-05-09 12:10:44 +0400

    basic first UI created

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import pt as parts  
+import pt as parts 
+from  PyQt5 import QtWidgets 
+import sys 
 
-a = int(input("Enter the number u want to partion:- "))
-ans = parts.getPartitions(a) 
+def window():
+    app = QtWidgets.QApplication(sys.argv) 
+    win = QtWidgets.QMainWindow()
+    win.setGeometry(200,200,300,300) 
+    win.setWindowTitle("Hello World")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("hi there")
+    label.move(50,50)
+    
+    win.show() 
+    sys.exit(app.exec_())
 
 
-print(len(ans))
+window()
+
+#a = int(input("Enter the number u want to partion:- "))
+#ans = parts.getPartitions(a) 
+#print(len(ans))
 
